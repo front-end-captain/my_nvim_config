fb247eb34890f83871e45c2a9c63deea1bfdffa1
front-end-captain <18810940932@163.com>
2022-12-19 20:32:21 +0800

    chore: virtcolumn char

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,11 +3,14 @@ local colorscheme = "gruvbox"
 -- vim.o.background = "light" -- or dark or light
 vim.o.background = "dark" -- or dark or light
 vim.o.termguicolors = true
-
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found！")
-	return
+  vim.notify("colorscheme " .. colorscheme .. " not found！")
+  return
 end
 
+vim.cmd([[highlight ColorColumn guibg=grey]])
+-- vim.g.virtcolumn_char = '▕' -- char to display the line
+vim.g.virtcolumn_char = "'" -- char to display the line
+vim.g.virtcolumn_priority = 10 -- priority of extmark
